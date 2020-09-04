4e883e493259054d58f04d756fc917884a460a4b
Nikita Morozov <dartsaben@gmail.com>
2020-09-04 12:30:46 +0500

    Fix mail sending

--- config/mail.php
+++ config/mail.php
@@ -81,6 +81,14 @@ return [
     */
 
     'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+    'stream' => [
+        'ssl' => [
+            'allow_self_signed' => true,
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+        ],
+    ],
+    'timeout' => 500,
 
     /*
     |--------------------------------------------------------------------------
